aea3b29200bb732df63657f6380b9bf0a1e57d4e
Anjan Borah <anjanborah@aol.com>
2014-05-07 22:06:23 +0530

    header.php edited

--- include/header.php
+++ include/header.php
@@ -1,8 +1,10 @@
 <?php
-    print '<a href="index.php" id="header_link_to_index">';
     print '<div id="header" class="header_footer">';
+	print '<a href="index.php" id="header_link_to_index">';
     print '<span id="header_name">SNMP Information Viewer</span><br>';
+	print '</a>';
+    print '<a href="https://github.com/anjanborah/SNMP_Information_viewer" id="link_to_github_project">';
     print '<span id="header_detail">Source code available at github.com</span><br>';
-    print '</div>';
     print '</a>';
+    print '</div>';
 ?>
